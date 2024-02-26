dceece0ebb5b5efb7104f98d1f25a6cdacb75121
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-26 14:40:53 -0300

    agrego imagenes

--- app/page.js
+++ app/page.js
@@ -1,25 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 async function fetchUsers() {
-  const res = await fetch("https://reqres.in/api/users")
-  const data = await res.json()
-  return data.data
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
 }
 
 async function HomePage() {
-
-  const users = await fetchUsers()
+  const users = await fetchUsers();
 
   return (
     <ul>
-      {users.map(user => (
-        <li key={user.id}>
+      {users.map((user) => (
+        <li key={user.id} className="bg-slate-500 mb-2 p-4 rounded-md flex justify-between">
           <div>
-            <h4>{user.id} {user.first_name} {user.last_name}</h4>
-           <p>email: {user.email}</p>
+            <h4 className="font-bold">
+              {user.id} {user.first_name} {user.last_name}
+            </h4>
+            <p className="text-slate-100">email: {user.email}</p>
           </div>
+          <img src={user.avatar} className="rounded-full w-20" />
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
 export default HomePage;
