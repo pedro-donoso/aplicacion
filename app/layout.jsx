a8c769666ae87d395bdc00cd7c83240fd538c0ca
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-27 10:19:59 -0300

    agrego pagina individual

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         <Navbar />
         <main className="container mx-auto flex justify-center items-center">
